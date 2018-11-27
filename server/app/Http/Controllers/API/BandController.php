d6101a53a349271f2f1d81cab85dd23500be3736
newaeonweb <fernando@newaeonweb.com.br>
2018-11-27 16:37:11 -0200

    added last step

---
+++ server/app/Http/Controllers/API/BandController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Band;
+use App\Http\Resources\BandResource;
+
+class BandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $listBands = Band::all();
+        // return $listBands;
+        return BandResource::collection(Band::with('albums')->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $createBand = Band::create($request->all());
+        return  $createBand;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $band = Band::findOrFail($id);
+        return $band;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $updateBandById = Band::findOrFail($id);
+        $updateBandById->update($request->all());
+        return $updateBandById;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $deleteBandById = Band::find($id)->delete();
+        return response()->json([], 204);
+    }
+}
